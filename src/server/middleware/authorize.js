781ac6e07182a22aa82ae13f90d637cd27339370
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-08-30 18:08:26 -0700

    implement auth

--- src/server/middleware/authorize.js
+++ src/server/middleware/authorize.js
@@ -1,3 +1,14 @@
+var logger = require('winston');
+var secret = process.env.YDM_API_SECRET;
+
+if (!secret) logger.warn('YDM_API_SECRET is unset. No authorization will take place!');
+else logger.info('API secret set. Make sure to use SSL!')
+
+
 module.exports = function(req, res, next) {
-  next()
+  if (!secret) next();
+  else {
+    if (req.get('X-Auth-Token') === secret) next();
+    else res.status(401).end();
+  }
 }
